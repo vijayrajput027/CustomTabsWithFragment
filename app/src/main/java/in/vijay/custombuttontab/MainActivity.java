4e4f3f830e0a9cc1bf1219100f3560ceec537528
vijayrajput027 <er.vijayrajput@gmail.com>
2016-02-06 17:07:24 +0530

    Initial commit

---
+++ app/src/main/java/in/vijay/custombuttontab/MainActivity.java
@@ -0,0 +1,49 @@
+package in.vijay.custombuttontab;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+        ProductFragment fragmentS1 = new ProductFragment();
+        fragmentTransaction.replace(android.R.id.content, fragmentS1);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/in/vijay/custombuttontab/ProductFragment.java
@@ -0,0 +1,57 @@
+package in.vijay.custombuttontab;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Vijay on 2/6/2016.
+ */
+public class ProductFragment extends Fragment{
+
+    private Button btnCustomer, btnProduct;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.product_fragment, container, false);
+
+        btnCustomer = (Button) rootView.findViewById(R.id.loginEnquiry);
+        btnProduct = (Button) rootView.findViewById(R.id.productId);
+
+
+        btnCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager
+                        .beginTransaction();
+                LoginEnquiryFragment fragmentS1 = new LoginEnquiryFragment();
+                fragmentTransaction.replace(android.R.id.content, fragmentS1);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btnProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager
+                        .beginTransaction();
+                ProductFragment fragmentS1 = new ProductFragment();
+                fragmentTransaction.replace(android.R.id.content, fragmentS1);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        return rootView;
+    }
+}
